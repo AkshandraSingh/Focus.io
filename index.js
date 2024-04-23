ab4ebd3d69f4222ee7ecd5a673babf9d09fd4c78
AkshandraSingh <nameste380@gmail.com>
2024-04-23 16:42:40 +0530

    Update: addedMenu

--- index.js
+++ index.js
@@ -1,8 +1,93 @@
 require('dotenv').config()
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu, shell } = require('electron')
 const path = require('path')
 
 const isProduction = process.env.PRODUCTION !== "development"
+
+const menuItems = [
+    {
+        label: "Options",
+        submenu: [
+            {
+                label: "About",
+                click: () => {
+                    shell.openExternal('https://akshandra.onrender.com');
+                },
+                accelerator: "CmdOrCtrl+Z",
+            },
+            {
+                label: "Github",
+                click: () => {
+                    shell.openExternal('https://github.com/AkshandraSingh/Focus.io');
+                },
+                accelerator: "CmdOrCtrl+G",
+            },
+            {
+                role: "Minimize"
+            },
+            {
+                label: "Quit",
+                click: () => {
+                    app.quit()
+                },
+                accelerator: "CmdOrCtrl+Q"
+            },
+        ]
+    },
+    {
+        label: "Windows",
+        submenu: [
+            {
+                label: "New Window",
+                click: async () => {
+                    const newWindow = new BrowserWindow({
+                        width: 500,
+                        maxWidth: 500,
+                        minWidth: 500,
+                        height: 400,
+                        maxHeight: 400,
+                        minHeight: 400,
+                    })
+                    await newWindow.loadFile(path.join(__dirname, "renderer", 'html', "index.html"))
+                },
+                accelerator: "CmdOrCtrl+N",
+            },
+            {
+                label: "About Window",
+                click: async () => {
+                    const newWindow = new BrowserWindow({
+                        width: 500,
+                        maxWidth: 500,
+                        minWidth: 500,
+                        height: 400,
+                        maxHeight: 400,
+                        minHeight: 400,
+                    })
+                    await newWindow.loadURL("https://akshandra.onrender.com")
+                },
+                accelerator: "CmdOrCtrl+N+A",
+            },
+            {
+                label: "Github Window",
+                click: async () => {
+                    const newWindow = new BrowserWindow({
+                        width: 500,
+                        maxWidth: 500,
+                        minWidth: 500,
+                        height: 400,
+                        maxHeight: 400,
+                        minHeight: 400,
+                    })
+                    await newWindow.loadURL("https://github.com/AkshandraSingh/Focus.io")
+                },
+                accelerator: "CmdOrCtrl+N+G",
+            }
+        ]
+    }
+]
+
+const menu = Menu.buildFromTemplate(menuItems)
+Menu.setApplicationMenu(menu)
 
 const createWindow = async () => {
     const window = new BrowserWindow({
@@ -12,6 +97,9 @@ const createWindow = async () => {
         height: 450,
         maxHeight: 450,
         minHeight: 450,
+        autoHideMenuBar: true,
+        frame: false,
+        // titleBarStyle: 'hidden',
     })
     if (isProduction) {
         window.webContents.openDevTools();
